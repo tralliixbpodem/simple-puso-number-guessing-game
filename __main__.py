a1fc3b5ecb84b25b406dfaf08a8c94b3f4677caf
tralliixbpodem <151377868+tralliixbpodem@users.noreply.github.com>
2023-11-18 13:24:18 +0000

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,10 @@
+# by kenan238
+from puso import run as puso_run_checker; puso_run_checker(disable=["one_line"]);
+from random import randint as rditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrdit
+r=rditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrditrdit(0,5);
+from os import system as systemsystemsystemsystem
+
+print('I have guessed a number from 0 to 5, what is it?');
+g=int(input('Guess: '));
+if g==r: print('You win!'); exit();
+if g!=r: print('No, try again'); systemsystemsystemsystem('python ' + __name__+'.py');
